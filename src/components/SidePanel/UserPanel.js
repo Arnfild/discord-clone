667f85b234dc7aded85a6db007c9413844824397
Arnfild <dmitrivolkov76@yandex.ru>
2020-12-23 00:30:22 +0300

    Added user profile picture to SidePanel component

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import firebase from "../../firebase"
 import { connect } from "react-redux";
-import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 
 class UserPanel extends React.Component {
     state = {
@@ -36,6 +36,8 @@ class UserPanel extends React.Component {
     }
     
     render() {
+        const { user } = this.state;
+        
         return (
             <Grid style={{ background: "#474747" }}>
                 <Grid.Column>
@@ -45,14 +47,19 @@ class UserPanel extends React.Component {
                             <Icon name="address book"/>
                             <Header.Content>DiscordClone</Header.Content>
                         </Header>
-                    </Grid.Row>
 
-                    {/*User Dropdown*/}
-                    <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                        <Dropdown trigger={
-                            <span>{this.state.user.displayName}</span>
-                        } options={this.dropdownOptions()} />
-                    </Header>
+                        {/*User Dropdown*/}
+                        <Header style={{ padding: '0.25em' }} as="h4" inverted>
+                            <Dropdown 
+                                trigger={
+                                <span>
+                                    <Image src={user.photoURL} spaced="right" avatar/>
+                                    {user.displayName}
+                                </span>} 
+                                options={this.dropdownOptions()} 
+                            />
+                        </Header>
+                    </Grid.Row>
                 </Grid.Column>
             </Grid>
         )
