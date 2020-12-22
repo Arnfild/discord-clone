58919834af7abbf9452f4e46ba07b279b80c55f1
Arnfild <dmitrivolkov76@yandex.ru>
2020-12-22 20:40:05 +0300

    Register class implementation

---
+++ src/firebase_sample.js
@@ -0,0 +1,19 @@
+// Below is the firebase config that must be filled for the app to work!
+
+// import firebase from 'firebase/app';
+// import "firebase/auth";
+// import "firebase/database";
+// import "firebase/storage";
+
+// var firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: "",
+//     appId: "",
+//     measurementId: ""
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase;
