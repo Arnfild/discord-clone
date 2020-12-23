6639fb04b9394fa57204c8436a50a87613a0919c
Arnfild <dmitrivolkov76@yandex.ru>
2020-12-23 12:00:38 +0300

    Implementing channels collection and channel add functionality

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -14,8 +14,8 @@ class SidePanel extends React.Component {
                 vertical
                 style={{ background: "#474747", fontSize: '1.2rem' }}
             >
-                <UserPanel currenUser={currentUser} />
-                <Channels />
+                <UserPanel currentUser={currentUser} />
+                <Channels currentUser={currentUser}/>
             </Menu>
         )
     }
