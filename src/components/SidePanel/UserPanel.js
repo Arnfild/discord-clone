a20a41380d2538463e005fc2f8555bc71b619f83
Arnfild <dmitrivolkov76@yandex.ru>
2020-12-23 21:39:27 +0300

    Implemented the emoji picker component

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -123,8 +123,8 @@ class UserPanel extends React.Component {
           <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
             {/* App Header */}
             <Header inverted floated="left" as="h2">
-              <Icon name="code" />
-              <Header.Content>DevChat</Header.Content>
+              <Icon name="address book" />
+              <Header.Content>DiscordClone</Header.Content>
             </Header>
 
             {/* User Dropdown  */}
